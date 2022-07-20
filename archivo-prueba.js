2ff5e1642d29ae59aedcd7cd9a8dcb5290e1a547
Nesqu1k22 <depaulanicolas2222@gmail.com>
2022-07-20 01:37:45 -0300

    new projects

--- archivo-prueba.js
+++ archivo-prueba.js
@@ -245,3 +245,204 @@ const convertirAMin = (minutos) => {
 	document.write (`resultado: ${horas} hs y ${valorMinutos} min`);
 	}
 }
+
+
+// Hacer un programa que solicite el ingreso de un número y que luego emita un
+//  cartel por pantalla aclarando si el mismo es múltiplo de 5.
+
+const multiploDe5 = (numero) => {
+	if (numero === 0) {
+		alert ("es 0")
+	}
+	if (numero  % 5 === 0) {
+		alert ("Es multiplo de 5");
+	}
+	else {
+		alert ("No es multiplo de 5");
+	}
+}
+
+// Hacer un programa que solicite el ingreso de dos números y luego calcular:
+//      a.La resta si el primero es mayor que el segundo.
+//      b.La suma si son iguales.
+//      c.El producto si el primero es menor.
+//  Se deberá emitir un cartel por pantalla con el resultado correspondiente.
+
+const operacionesSegun = (a,b) => {
+	
+	var final = null;
+
+	if (a > b) {
+		final = a - b;
+	}
+	if (a === b) {
+		final = a + b;
+	}
+	if (b > a) {
+		final = a * b;
+	}
+	alert (`el resultado es ${final}`)
+}
+
+// Hacer un programa para ingresar dos números.Si el segundo es distinto de
+//  cero, calcular la división del primero por el segundo y mostrar el resultado por
+//  pantalla; caso contrario, emitir un cartel aclarando que no se puede dividir por
+//  cero.
+
+const division = (a,b) => {
+	final = null;
+	if (b != 0){
+		final = a / b;
+		alert (`El resultado es ${final}`);
+	}
+	else {
+		alert ("no se puede dividir por 0");
+	}
+}
+
+// Un importante negocio de desinfectante líquido realiza descuentos
+//  dependiendo de la cantidad de litros vendidos según la siguiente escala:
+//      a. Si vende menos de 100 litros, no hay descuento.
+//      b. Si vende entre 101 y 300 litros, el descuento es del 10%.
+//      c. Si vende entre 301 y 500 litros, el descuento es del 15%.
+//      d. Finalmente, si la venta es de más de 500 litros, el descuento es del 25%.
+//  Hacer un programa que solicite el ingreso del importe total de la venta y la
+//  cantidad de litros vendidos, calcule y emita el importe con el descuento
+//  aplicado.
+
+const descuentoPor = (precio, litros) => {
+ 
+	if (101 > litros) {
+		alert (`usted compro ${litros} litros, el valor de su compra es ${precio}`);
+	}
+	else {
+		
+		if (litros > 100 && 301 > litros) {
+			porcentaje = 10;
+		}
+		if (litros > 300 && 501 > litros) {
+			porcentaje = 15;
+		}
+		if (litros > 500) {
+			porcentaje = 25;
+		}
+
+		importTotal = precio - ((precio * porcentaje) / 100 );
+
+	 alert (`usted compro ${litros} litros, el valor de su compra es ${importTotal}`);
+	}
+}
+
+// Hacer un programa que solicite el ingreso de las notas del primer parcial y del
+//  segundo parcial de una alumna de programación.El programa deberá analizar
+//  las notas y emitir la situación de la alumna según la siguiente escala:
+//      a. Si tiene 8 o más en ambos parciales,emitir "aprobación directa".
+//      b. Si no tiene 8 o más en ambos pero tiene aprobados ambos parciales (se
+//          aprueba con 6 o más), emitir "rinde examen final".
+//      c. Si tiene menos de 6 en alguno de los dos parciales, emitir "debe
+//          recuperar".
+//  El programa debe emitir solo un cartel, el que corresponda.
+
+const estatusDeAlumno = (a,b) =>{
+	if (6 > a || 6 > b) {
+		alert ("debe recuperar");
+	}
+	if (a > 7 && b > 7) {
+		alert ("aprobacion directa");
+	}
+	else {
+		alert ("rinde examen final");
+	}
+}
+
+// Hacer un programa para ingresar por teclado la longitud de los tres lados de un
+// triángulo y que luego determine e informe con un cartel aclaratorio a qué tipo
+//  de triángulo corresponde:
+//      a. Equilátero: cuando los tres lados sean iguales.
+//      b. Isósceles: cuando dos de los tres lados sean iguales.
+//      c. Escaleno: cuando todos los lados sean distintos.
+
+const definirTriangulo = (a,b,c) =>{
+
+	 if (a === b && a === c) {
+	 	alert ("es un triángulo equilátero");
+	 }
+	 if (a === b || a === c || b === c) {
+	 	alert ("es un triangulo Isósceles");
+	 }
+	 else {
+	 	alert ("es un triangulo Escaleno");
+	 }
+
+}
+
+//Hacer un programa para ingresar 4 números. Luego analizar e informar por
+//pantalla si los mismos se encuentran ordenados de forma decreciente.
+
+const ordenados = (a,b,c,d) => {
+	if (a > b && b > c && c > d){
+		alert ("estan ordenados en forma decreciente");
+	}
+	else {
+		alert ("no estan ordenados de forma decreciente");
+	}
+}
+
+//El negocio de desinfectante antes mencionado vende además detergente
+//suelto, y los precios se aplican según la siguiente escala:
+//   a. 25 ARS por litro los primeros 50 litros.
+//   b. 20 ARS por litro si la venta es de 51 a 200 litros.
+//   c. 15 ARS por litro si la venta es de 201 a 500 litros.
+//   d. 10 ARS por litro si la venta es de más de 500 litros.
+//Además, si paga en efectivo, tiene un adicional del 10% sobre el importe final.
+//Hacer un programa que solicite la cantidad de litros vendidos y el tipo de pago
+//(ingresará 1 si paga en efectivo y 0 con cualquier otro medio de pago) y calcule
+//y emita por pantalla el monto final a abonar por el cliente.
+
+const valorLitro = (litros) =>{
+	let pago = prompt (`si abona en efectivo ingrese "1" de lo contrario ingrese "0"`); 
+	if (51 > litros){
+		precioLitro = 25;
+	}
+	if (201 > litros){
+		precioLitro = 20;
+	}
+	if (501 > litros){
+		precioLitro = 15;
+	}
+	if (litros > 500){
+		precioLitro = 10;
+	}
+	else if (pago === 1){
+
+	}
+	precioFinal = 
+}
+
+//Hacer un programa que solicite cuatro números y emitir un cartel aclaratorio si
+//son todos iguales entre sí, caso contrario, no emitir nada.
+
+const iguales4 = (a,b,c,d) => {
+	if (a === b && b === c && c === d) {
+		alert ("son todos iguales entre si");
+	}
+}
+
+//Hacer un programa para ingresar tres números y luego mostrarlos ordenados
+//de menor a mayor.
+
+const menorAMayor = (a,b,c) => {
+	let numeros = [a,b,c];
+ 	numeros.sort((a,b) => {
+ 		return a - b
+ 	} );  
+ 	alert (numeros);
+}
+//Hacer un programa para ingresar tres números y emitir un cartel aclaratorio si
+//la suma de los dos primeros es mayor al producto del segundo con el tercero.
+
+const elMayor = (a,b,c) =>{
+if ((a + b) > (b * c)){
+	alert ("la suma del 1er y 2do numero es mayor a la multiplicacion del 2do con el 3ro")
+}
+}
